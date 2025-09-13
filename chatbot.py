2cf23d04220fa8fab06ce54795e898a7bcc13cc5
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2025-09-13 12:22:27 +0600

    model generating weird and repititive responses

--- chatbot.py
+++ chatbot.py
@@ -7,16 +7,18 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 conversation_history = []
 
-history_string = "\n".join(conversation_history)
+while True:
 
-input_text ="hello, how are you doing?"
-inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+    history_string = "\n".join(conversation_history)
+    
+    input_text = input("> ")
+    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
 
-outputs = model.generate(**inputs)
+    outputs = model.generate(**inputs)
 
 
-response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
-print(response) 
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    print(response) 
 
-conversation_history.append(input_text)
-conversation_history.append(response)
+    conversation_history.append(input_text)
+    conversation_history.append(response)
